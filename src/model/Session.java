2ec69f7937c7e5da5c360b122bfa187d6f4f9bdc
munchQuim <qpinedasotes@gmail.com>
2024-12-09 17:41:47 +0100

    a

---
+++ src/model/Session.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import model.Film;
+import model.Room;
+import java.time.LocalDateTime;
+/**
+ *
+ * @author joaquimpinsot
+ */
+public class Session {
+    private Film pelicula;
+    private LocalDateTime horaInicio;
+    private LocalDateTime horaFinal;
+    private Room sala; // o hacer como referencia;
+    
+}
